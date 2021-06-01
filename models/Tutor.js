a882327aa367eb8c34ddc098ad061a37576006e4
lewy192 <lewis.james.hill@outlook.com>
2021-06-01 20:20:39 +1000

    added: User Model, Updated: Tutor Model

--- models/Tutor.js
+++ models/Tutor.js
@@ -1,52 +1,64 @@
 const { Model, DataTypes } = require("sequelize");
 
+const sequelize = require("connection location goes here");
+
 class Tutor extends Model {}
 
-Tutor.init({
-    id: {
-        type: DataTypes.NUMBER.UNSIGNED,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-    },
-    userId: {
-        type: DataTypes.NUMBER.UNSIGNED,
-        allowNull: false,
-        references: {
-            model: "user",
-            key: "id",
+Tutor.init(
+    {
+        id: {
+            type: DataTypes.NUMBER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.NUMBER.UNSIGNED,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id",
+            },
+        },
+        isFrontEnd: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        isBackEnd: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        css: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        javaScript: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        html: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        sql: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        node: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+        react: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
         },
     },
-    isFrontEnd: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    isBackEnd: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    css: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    javaScript: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    html: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    sql: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    node: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-    react: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-});
+
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: "tutor",
+        underscored: true,
+    }
+);
