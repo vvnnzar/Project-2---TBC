9c8540f5060eb2dabbb3b2d7ae972f4906638d89
Kate McColl <kate@katemccollart.com>
2021-06-04 11:07:56 +1000

    add: handlebars and route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,26 @@
 const router = require("express").Router();
-// const { User, IsTutor, Comment, Question, Reputation, QuizResult } = require('../models');
+const { User, IsTutor, Comment, Question, Reputation, QuizResult } = require('../models');
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-    res.render("homepage");
-    return;
+    try {
+        const questionData = await Question.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const questions = questionData.map((question) => question.get({ plain: true }));
+
+        res.render('homepage', {
+            questions,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.get("/login", (req, res) => {
