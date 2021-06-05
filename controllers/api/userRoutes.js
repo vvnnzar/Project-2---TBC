cd51848967a1ed42921e1ed5adb53d8d315a129b
Viv Zara <vzara88@hotmail.com>
2021-06-05 17:34:07 +1000

    Minor updates

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,74 +3,89 @@ const bcrypt = require("bcryptjs");
 const { User } = require("../../models");
 
 router.post("/", async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
+  try {
+    const userData = await User.create(req.body);
 
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
 
-            res.status(200).json(userData);
-        });
-    } catch (err) {
-        res.status(400).json(err);
-    }
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post("/signup", async (req, res) => {
-    console.log(req.body);
-    // hashing password
-    // req.body.password = await bcrypt.hash(req.body.password, 10);
-    // creating User Model after registration
-    if (!req.body.password === req.body.confirmPassword) {
-        // TODO: trigger Handle Bars if statment
-    }
+  console.log(req.body);
+  // hashing password
+  // req.body.password = await bcrypt.hash(req.body.password, 10);
+  // creating User Model after registration
+  if (!req.body.password === req.body.confirmPassword) {
+    // TODO: trigger Handle Bars if statment
+  }
 
-    // TODO: find more optiam solution for next two ifd statements
-    if (!req.body.isTutor) {
-        req.body.isTutor = false;
-    }
+  // TODO: find more optiam solution for next two ifd statements
+  if (!req.body.isTutor) {
+    req.body.isTutor = false;
+  }
 
-    if (req.body.isTutor === "on") {
-        req.body.isTutor = true;
-    }
-    const userData = {
-        username: req.body.username,
-        firstName: req.body.firstname,
-        lastName: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password,
-        isTutor: req.body.isTutor,
-    };
-    const userRegistration = await User.create(userData);
-    req.session.save(() => {
-        req.session.user_id = userRegistration.id;
-        req.session.logged_in = true;
+  if (req.body.isTutor === "on") {
+    req.body.isTutor = true;
+  }
+  const userData = {
+    username: req.body.username,
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    email: req.body.email,
+    password: req.body.password,
+    isTutor: req.body.isTutor,
+  };
+  const userRegistration = await User.create(userData);
+  req.session.save(() => {
+    req.session.user_id = userRegistration.id;
+    req.session.logged_in = true;
 
-        res.status(201).json(userRegistration);
-    });
-    // users.push(req.body);
-    // res.status(201).send(req.body);
-    // handle unique username
+    res.status(201).json(userRegistration);
+  });
+  // users.push(req.body);
+  // res.status(201).send(req.body);
+  // handle unique username
 });
 
 router.post("/login", async (req, res) => {
-    const currentUser = await User.findOne({
-        where: { username: req.body.username },
+  const currentUser = await User.findOne({
+    where: { username: req.body.username },
+  });
+  if (!currentUser) {
+    res.status(404).send("Incorrect User name, would you like to sign up?");
+  }
+  console.log("this is current" + currentUser);
+  try {
+    // if (await bcrypt.compare(req.body.password, currentUser.password)) {
+    res.status(302).redirect("/");
+    // } else {
+    //     res.status(404).send("Incorrect Password");
+    // }
+  } catch (err) {
+    res.status(500).send(`${err}`);
+  }
+});
+
+//profile page update user data
+router.put("/user/:id", (req, res) => {
+  // update product data
+  try {
+    User.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
     });
-    if (!currentUser) {
-        res.status(404).send("Incorrect User name, would you like to sign up?");
-    }
-    console.log("this is current" + currentUser);
-    try {
-        // if (await bcrypt.compare(req.body.password, currentUser.password)) {
-        res.status(302).redirect("/");
-        // } else {
-        //     res.status(404).send("Incorrect Password");
-        // }
-    } catch (err) {
-        res.status(500).send(`${err}`);
-    }
+    res.status(200).json({ message: "Profile successfully updated!" });
+  } catch (err) {
+    res.status(500).send(`${err}`);
+  }
 });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,57 @@
 const router = require("express").Router();
-// const { User, IsTutor, Comment, Question, Reputation, QuizResult } = require('../models');
+const {
+  User,
+  IsTutor,
+  Comment,
+  Question,
+  Reputation,
+  QuizResult,
+} = require("../models");
 const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
-    res.render("homepage");
-    return;
+  res.render("homepage");
+  return;
 });
 
 router.get("/login", (req, res) => {
-    // if (req.session.logged_in) {
-    //     res.redirect("/");
-    //     return;
-    // }
-    res.render("login");
+  // if (req.session.logged_in) {
+  //     res.redirect("/");
+  //     return;
+  // }
+  res.render("login");
 });
 
 router.get("/signup", (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect("/");
-        return;
-    }
-    res.render("signup");
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+//profile
+router.get("/profile", async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const currentUser = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [
+        { model: Reputation },
+        { model: QuizResult },
+        { model: IsTutor },
+      ],
+    });
+
+    const user = currentUser.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
