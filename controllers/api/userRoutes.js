842499fcb4ccf7cfdb49fbc4a35f01bc30b58f13
lewy192 <lewis.james.hill@outlook.com>
2021-06-05 16:34:50 +1000

    minor adjustments to routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,32 +1,30 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const { User } = require("../../models");
+require("dotenv").config();
 
-router.post("/", async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
+// router.post("/", async (req, res) => {
+//     try {
+//         const userData = await User.create(req.body);
 
-        req.session.save(() => {
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
+//         req.session.save(() => {
+//             req.session.user_id = userData.id;
+//             req.session.logged_in = true;
 
-            res.status(200).json(userData);
-        });
-    } catch (err) {
-        res.status(400).json(err);
-    }
-});
+//             res.status(200).json(userData);
+//         });
+//     } catch (err) {
+//         res.status(400).json(err);
+//     }
+// });
 
 router.post("/signup", async (req, res) => {
     console.log(req.body);
     // hashing password
-    // req.body.password = await bcrypt.hash(req.body.password, 10);
+    const salt = await bcrypt.genSalt(10);
+    req.body.password = await bcrypt.hash(req.body.password, salt);
     // creating User Model after registration
-    if (!req.body.password === req.body.confirmPassword) {
-        // TODO: trigger Handle Bars if statment
-    }
-
-    // TODO: find more optiam solution for next two ifd statements
+    // TODO: find more optiam solution for next two if statements
     if (!req.body.isTutor) {
         req.body.isTutor = false;
     }
@@ -47,8 +45,10 @@ router.post("/signup", async (req, res) => {
         req.session.user_id = userRegistration.id;
         req.session.logged_in = true;
 
-        res.status(201).json(userRegistration);
+        res.status(201);
     });
+
+    // TODO: if (isTutor === true) {res.render('quiz')}
     // users.push(req.body);
     // res.status(201).send(req.body);
     // handle unique username
@@ -61,16 +61,29 @@ router.post("/login", async (req, res) => {
     if (!currentUser) {
         res.status(404).send("Incorrect User name, would you like to sign up?");
     }
-    console.log("this is current" + currentUser);
     try {
-        // if (await bcrypt.compare(req.body.password, currentUser.password)) {
-        res.status(302).redirect("/");
-        // } else {
-        //     res.status(404).send("Incorrect Password");
-        // }
+        if (await bcrypt.compare(req.body.password, currentUser.password)) {
+            res.status(302).redirect("/");
+        } else {
+            res.status(404).send("Incorrect Password");
+        }
     } catch (err) {
         res.status(500).send(`${err}`);
     }
 });
+
+// router.post("/tutor", async (req, res) => {
+//     const plainTutor = await User.findOne({
+//         where: { username: req.body.tutorUsername },
+//     });
+//     if (!searchedTutor) {
+//         // TODO: replace .send with a view modification
+//         res.status(404).send(
+//             "This Tutor does not exist or that user is not a tutor"
+//         );
+//     }
+//     const plainTutor = searchedTutor.get({ plain: true });
+//     res.render("tutors", { plainTutor });
+// });
 
 module.exports = router;
