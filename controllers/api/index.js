14b33bc898dc9c531604e46b916c794cd9491f70
Kate McColl <kate@katemccollart.com>
2021-06-07 15:54:52 +1000

    add: sidebar for notes, comment on for questions, css styling

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const questionRoutes = require('./questionRoutes');
-// const commentRoutes = require('./commentRoutes');
+const commentRoutes = require('./commentRoutes');
+const noteRoutes = require ('./noteRoutes')
 
 router.use("/users", userRoutes);
 router.use("/questions", questionRoutes);
-// router.use('/comments', commentRoutes);
+router.use("/comments", commentRoutes);
+router.use("/notes", noteRoutes);
 
 module.exports = router;
---
+++ models/Note.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Note extends Model {}
+
+Note.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        note_title: {
+            type: DataTypes.STRING(52),
+            allowNull: false,
+        },
+        note_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            allowNull: false,
+            references: {
+                model: "User",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+            timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "note",
+    }
+);
+
+module.exports = Note;
