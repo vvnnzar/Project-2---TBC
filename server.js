1ba41580ee2338f124ff316c28d92926681f1ff0
lewy192 <lewis.james.hill@outlook.com>
2021-06-08 13:37:03 +1000

    added client-sessions

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
 const express = require("express");
 const path = require("path");
 const routes = require("./controllers");
-const session = require("express-session");
+const session = require("client-sessions");
 const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
+require("dotenv").config();
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-const sess = {
-    secret: process.env.SESSION_SECRET_KEY,
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
-};
-
-app.use(session(sess));
+app.use(
+    session({
+        cookieName: "session",
+        secret: process.env.SESSION_SECRET_KEY,
+        duration: 2 * 24 * 60 * 60 * 1000,
+        activeDuration: 15 * 60 * 1000,
+        cookie: {
+            httpOnly: true,
+            maxAge: 2 * 24 * 60 * 60 * 1000,
+            sameSite: "strict",
+            //TODO: ADD secure when put into procution,
+            // secure:true,
+        },
+    })
+);
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -34,6 +38,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
 });
