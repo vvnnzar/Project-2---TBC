64a5519f5dd9a108c91f7115b7cd10f441efe812
Kate McColl <kate@katemccollart.com>
2021-06-09 22:52:39 +1000

    add: delete comment

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -22,4 +22,29 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const payload = auth.extractPayload(req, res);
+        let {
+            logged_in: logged_in,
+            userid: user_id,
+            username: name,
+        } = payload || { logged_in: false };
+        const newComment = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: user_id,
+            },
+        });
+
+        if (!newComment) {
+            res.status(404).json({message: 'No question found with this id'});
+            return;
+        }
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -74,11 +74,14 @@ router.get(
             }
 
             const question = questionData.get({ plain: true });
+            question.comments.forEach((comment) => {
+                comment.is_owner = user_id === comment.user_id;
+            });
             const isOwner = question.user_id === user_id;
             res.render("question", {
                 ...question,
                 is_owner: isOwner,
-                logged_in: logged_in,
+                logged_in: logged_in
             });
         } catch (err) {
             res.status(500).json(err);
