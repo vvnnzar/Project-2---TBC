c6c0d9b6bf65c29ec64384568ffca2e7145bbf6d
Kate McColl <kate@katemccollart.com>
2021-06-12 16:38:40 +1000

    add: postgres config

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+    const config = { dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }};
+    sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
     sequelize = new Sequelize(
         process.env.DB_NAME,
